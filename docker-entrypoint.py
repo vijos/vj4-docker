440de1bdb1892c49ef88c1ad7a1b94b06ab858f2
Soha Jin <soha@lohu.info>
2018-11-17 11:16:27 +0800

    Add docker-compose.yml

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -8,17 +8,17 @@ arg_list = args[1:]
 payload = []
 
 if cmd.startswith('vj4.'):
-	payload = [ sys.executable, '-m', cmd ]
-	if cmd == 'vj4.server':
-		for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
-			if k.startswith('VJ_'):
-				if k == 'VJ_CMDLINE_FLAGS': # for setting bool flags like `--debug` or `--no-pretty`
-					payload.extend(v.split())
-				else:
-					payload.append('--' + k[3:].lower().replace('_','-'))
-					payload.append(v)
-	payload.extend(arg_list)
+    payload = [ sys.executable, '-m', cmd ]
+    if cmd == 'vj4.server':
+        for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
+            if k.startswith('VJ_'):
+                if k == 'VJ_CMDLINE_FLAGS': # for bool flags like `--debug` or `--no-pretty`
+                    payload.extend(v.split())
+                else:
+                    payload.append('--' + k[3:].lower().replace('_','-'))
+                    payload.append(v)
+    payload.extend(arg_list)
 else:
-	payload = args
+    payload = args
 
 subprocess.call(payload)
