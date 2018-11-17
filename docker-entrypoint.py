6417314ebd8560ece2df75132afd41491161cf20
Soha Jin <soha@lohu.info>
2018-11-17 14:28:37 +0800

    Fix passing envs as args

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -2,21 +2,25 @@
 import os, sys
 import subprocess
 
+vj4_global_args = [
+    'db-host',
+    'db-name',
+]
 args = sys.argv[1:]
-cmd = args[0]
+cmd = args[0] if len(args) > 0 else 'vj4.server'
 arg_list = args[1:]
 payload = []
 
 if cmd.startswith('vj4.'):
     payload = [ sys.executable, '-m', cmd ]
-    if cmd == 'vj4.server':
-        for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
-            if k.startswith('VJ_'):
-                if k == 'VJ_CMDLINE_FLAGS': # for bool flags like `--debug` or `--no-pretty`
-                    payload.extend(v.split())
-                else:
-                    payload.append('--' + k[3:].lower().replace('_','-'))
-                    payload.append(v)
+    for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
+        if k.startswith('VJ_'):
+            param_name = k[3:].lower().replace('_','-')
+            if k == 'VJ_CMDLINE_FLAGS': # for bool flags like `--debug` or `--no-pretty`
+                payload.extend(v.split())
+            elif (param_name in vj4_global_args) or (cmd == 'vj4.server'):
+                payload.append('--' + param_name)
+                payload.append(v)
     payload.extend(arg_list)
 else:
     payload = args
