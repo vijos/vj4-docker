a6114930ddea13a509649c5146a102fc9ba4f99c
Soha Jin <soha@lohu.info>
2018-12-11 11:44:01 +0800

    Follow vj4 for MongoDB auth options

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -3,10 +3,11 @@ import os, sys
 
 vj4_global_args = [
     'db-host',
+    'db-port',
     'db-name',
     'db-username',
     'db-password',
-    'db-auth'
+    'db-auth-source'
 ]
 args = sys.argv[1:]
 cmd = args[0] if len(args) > 0 else 'vj4.server'
@@ -17,7 +18,7 @@ if cmd.startswith('vj4.'):
     payload = [ sys.executable, '-m', cmd ]
     for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
         if k.startswith('VJ_'):
-            param_name = k[3:].lower().replace('_','-')
+            param_name = k[3:].lower().replace('_', '-')
             if (param_name in vj4_global_args) or (cmd == 'vj4.server'):
                 if k == 'VJ_CMDLINE_FLAGS': # for bool flags like `--debug` or `--no-pretty`
                     payload.extend(v.split())
@@ -28,4 +29,4 @@ if cmd.startswith('vj4.'):
 else:
     payload = args
 
-os.execvp(payload[0],payload)
+os.execvp(payload[0], payload)
